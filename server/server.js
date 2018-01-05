38947cb97c9f091f8a5e9cf0478429cc41155a8a
naveen62 <coolnaveen62@gmail.com>
2018-01-05 06:06:40 -0800

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -23,7 +23,12 @@ app.post('/todos', (req, res) => {
 })
 
 app.get('/todos', (req, res) => {
-    
+    Todo.find({}).then((todos) => {
+        res.send({todos})
+    }, (err) => {
+        res.status(400);
+        res.send(err)
+    })
 })
 
 app.listen(3000, () => {
