3d5e974eca45f37b330b936ad06a01992d99e0cc
naveen62 <coolnaveen62@gmail.com>
2018-01-05 23:33:00 -0800

    heroku deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'); 
 
 mongoose.Promise = global.Promise
-mongoose.connect(`mongodb://${process.env.DATABASEURL}` || "mongodb://127.0.0.1:27017/todoApp", {useMongoClient: true})
+mongoose.connect(process.env.DATABASEURL || "mongodb://127.0.0.1:27017/todoApp")
 
 module.exports = {
     mongoose,
